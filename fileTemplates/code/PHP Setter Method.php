236e133a196b95f25f7f7581e274d53a52b0b35c
liuende <shui501@126.com>
2018-06-03 10:53:17 +0800

    PS-2017.2.4 <liuende@liuende-MacBook Create markdown.xml, IntelliLang.xml, Nette.icls, jQuery.xml, project.default.xml, diff.xml, Default copy.xml, Default.xml, ignore.xml, find.xml, knp_frontend.xml, jQuery.icls, liuende.xml, NetteTester.icls, path.macros.xml, Darcula copy.icls, github_settings.xml, symfony.xml, PHP.xml, hg.xml, PHP Getter Method.php, Default.xml, PHP.xml, JavaScript.xml, ihospital.xml, sublime.icls, Doctrine.xml, PHP Setter Method.php, mystyle.xml, keymap.xml, editor.rich.copy.xml, editor.xml, applicationLibraries.xml, webServers.xml, debugger.xml, baseRefactoring.xml, ide.general.xml, composerJson.xml, ea_extended.xml, git.xml, other.xml, Nette.xml, Twig.xml, ui.lnf.xml, filetypes.xml, php.xml, projectView.xml, plugin_ui.xml, PHP fprochazka.xml, watcherDefaultTasks.xml, colors.scheme.xml, html_xml.xml, notifications.xml, databaseDrivers.xml, customization.xml, NetteTester.xml, PHP File Header.php, intentionSettings.xml, laf.xml, vcs.xml, databaseSettings.xml, myDefaul.xml

---
+++ fileTemplates/code/PHP Setter Method.php
@@ -0,0 +1,11 @@
+/**
+ * @param $${PARAM_NAME}
+ */
+public ${STATIC} function set${NAME}($${PARAM_NAME})
+{
+#if (${STATIC} == "static")
+    self::$${FIELD_NAME} = $${PARAM_NAME};
+#else
+    $this->${FIELD_NAME} = $${PARAM_NAME};
+#end
+}
